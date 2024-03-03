fa80a3a51d3856cb378335c4c0457d995cee12f3
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-03-03 20:56:18 +0530

    map using Function calling

--- App.js
+++ App.js
@@ -1,47 +1,15 @@
 
 import './App.css';
 import Post from './Components/Post';
-const Data=[
-  {        //selecting area can be reapeated by Press control+alt key and press down key 
-    fname:"Chathura",
-    job:"react-Developer",
-    university:"university of Peradeniya",
-    school:"R/st.Aloysious College"
-  },
-  {
-    fname:"isuru",
-    job:"react-Developer",
-    university:"university of Peradeniya",
-    school:"R/st.Aloysious College"
-  },
-  {
-    fname:"isuru",
-    job:"react-Developer",
-    university:"university of Peradeniya",
-    school:"R/st.Aloysious College"
-  },
-  {
-    fname:"sandarwuan",
-    job:"Front-end-Developer",
-    university:"university of ruhuna",
-    school:"R/st.Aloysious College"
-  },
-  {
-    fname:"Niman",
-    job:"full-stack-Developer",
-    university:"university of Moratuwa",
-    school:"R/st.Aloysious College"
-  },
-  {
-    fname:"Thilini Athukorala",
-    job:"back-end--Developer",
-    university:"university of kelaniya",
-    school:"R/st.Aloysious College"
-  },
-]
+import Data from './data'
 
 
 function App() {
+
+  const MainBlock= Data.map(({fname,school,job,university})=>{
+    return <Post key={fname} name={fname} school={school} university={university} job={job}/>
+  })
+
   return (
     <>
     {/*                           ==================== Normal Data Mapping Method ================================
@@ -66,15 +34,28 @@ function App() {
 
    */}
 
-{/* ====================avoid Error beause not used Key varaiable================================
+   {/*
+  ====================avoid Error beause not used Key varaiable================================
 map method magin access karann puluwan parameter 3k(looping index,complete Array,return Element)
-***key ekak add kirima aniwaren sidu kalyuthuri,nattm identifer karagann ba react ekat componnents */}
+***key ekak add kirima aniwaren sidu kalyuthuri,nattm identifer karagann ba react ekat componnents 
     {
       Data.map(({fname,school,job,university},index)=>{
         return <Post key={index} name={fname} school={school} university={university} job={job}/>
       })
     }
-   
+   {
+      Data.map(({fname,school,job,university})=>{
+        return <Post key={fname} name={fname} school={school} university={university} job={job}/>
+      })
+    }
+
+
+    */}
+
+
+
+    {/* By Function Calling */}
+              {MainBlock}
 
 
 
---
+++ data.js
@@ -0,0 +1,46 @@
+
+//another export method  -         export const Data=[]
+//how to import this     -         import {Dara } from "/path"
+
+
+const Data = [
+  {
+    //selecting area can be reapeated by Press control+alt key and press down key
+    fname: "Chathura",
+    job: "react-Developer",
+    university: "university of Peradeniya",
+    school: "R/st.Aloysious College",
+  },
+  {
+    fname: "isuru",
+    job: "react-Developer",
+    university: "university of Peradeniya",
+    school: "R/st.Aloysious College",
+  },
+  {
+    fname: "isuru sandaruean",
+    job: "react-Developer",
+    university: "university of Peradeniya",
+    school: "R/st.Aloysious College",
+  },
+  {
+    fname: "sandarwuan",
+    job: "Front-end-Developer",
+    university: "university of ruhuna",
+    school: "R/st.Aloysious College",
+  },
+  {
+    fname: "Niman",
+    job: "full-stack-Developer",
+    university: "university of Moratuwa",
+    school: "R/st.Aloysious College",
+  },
+  {
+    fname: "Thilini Athukorala",
+    job: "back-end--Developer",
+    university: "university of kelaniya",
+    school: "R/st.Aloysious College",
+  },
+];
+
+export default Data;
